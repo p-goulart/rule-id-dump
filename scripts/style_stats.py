1b352bf1988e906346ae22a2ac2db9bea613d424
affemitkaraffe <48110099+affemitkaraffe@users.noreply.github.com>
2023-07-19 17:20:06 +0200

    Add information about rule totals to style summaries (#20)
    * try to improve summary
    
    * fix comment
    
    * add more counts and add summary over all languages
    
    * try to fix indent
    
    * improve exception handling
    
    * use logger

--- scripts/style_stats.py
+++ scripts/style_stats.py
@@ -4,6 +4,7 @@ from lib.logger import logger_wrapper
 from lib.rule_dump import RuleDump
 from lib.constants import *
 from pandas import DataFrame
+import pandas as pd
 import collections
 
 
@@ -30,6 +31,12 @@ def rows_from_rule(rule, repo_name, row_list):
             'untagged',
             repo_name
         ])
+        return
+    row_list.append([
+        rule.id,
+        'tagged',
+        repo_name
+    ])
     for tt in rule.tone_tags:
         row_list.append([
             rule.id,
@@ -53,11 +60,32 @@ def compare_dfs(df_to, df_from):
     return "\n".join([f"{key},{value}" for key, value in coll_to.items()])
 
 
+def summarize_tts(df, logger):
+    repo_grps = df.groupby(by='repo')
+    summary = repo_grps['tone_tags'].value_counts().reset_index(name='count')
+
+    try:
+        total_tts = repo_grps.apply(lambda x: x[(x['tone_tags'] != 'tagged') & (x[
+            'tone_tags'] != 'untagged')]['tone_tags'].count()).reset_index(name='count')
+        total_tts["tone_tags"] = "total"
+    except TypeError as e:
+        logger.warning(e.__str__() + "\n*total* is set to 0 for all repos")
+        total_tts = pd.DataFrame([['os', 'total', 0], ['premium', 'total', 0]], columns=['repo', 'tone_tags', 'count'])
+
+    unique_rules = repo_grps['id'].nunique().to_frame().reset_index()
+    unique_rules = unique_rules.rename(columns={'id': 'count'})
+    unique_rules["tone_tags"] = "unique_rules"
+
+    summary = pd.concat([summary, total_tts, unique_rules], axis=0).sort_values(['repo', 'tone_tags'])
+    return summary
+
+
 def __main__():
     cli = CLI()
     logger = logger_wrapper(cli.parser.prog, cli.args.verbosity)
     logger.debug(f"Starting script...\nInvoked with options: {cli.args}")
     headers = ['id', 'tone_tags', 'repo']
+    all_rows_to = []
     for locale in LOCALES:
         locale_dir = os.path.join(cli.args.out_dir, locale)
         os.makedirs(locale_dir, exist_ok=True)
@@ -69,14 +97,22 @@ def __main__():
         for repo_name, repo_dir in REPOS.items():
             df_from_repo(cli.args.to_dir, repo_dir, repo_name, locale, rows_to)
             df_from_repo(cli.args.from_dir, repo_dir, repo_name, locale, rows_from)
+        all_rows_to.extend(rows_to)
         df_to = DataFrame(rows_to, columns=headers)
         df_from = DataFrame(rows_from, columns=headers)
-        summary = df_to.groupby(by='repo')['tone_tags'].value_counts().to_string()
-        logger.debug(summary)
-        open(summary_filepath, 'w').write(summary)
+        locale_summary = summarize_tts(df_to, logger)
+
+        logger.debug(locale_summary)
+        open(summary_filepath, 'w').write(locale_summary.to_string(index=False))
         comparison = compare_dfs(df_to, df_from)
         logger.debug(comparison)
         open(added_filepath, 'w').write(comparison)
+
+    df_all_to = DataFrame(all_rows_to, columns=headers)
+
+    all_summary = summarize_tts(df_all_to, logger)
+    all_filepath = os.path.join(cli.args.out_dir, 'all_time_summary.txt')
+    open(all_filepath, 'w').write(all_summary.to_string(index=False))
 
 
 __main__()
