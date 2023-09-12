f4f1a481519eff495be3449a0311b27e5a3bac20
affemitkaraffe <s4jozach@uni-trier.de>
2023-09-12 11:28:27 +0200

    only print all comments (drop diff)

--- scripts/cross_ling_appl.py
+++ scripts/cross_ling_appl.py
@@ -14,13 +14,13 @@ class CLI:
         )
         self.parser.add_argument('-v', '--verbosity', default='INFO')
         self.parser.add_argument('-o', '--out-dir', default='lt-cross-language-applicability')
-        self.parser.add_argument('-f', '--from-dir')
+        # self.parser.add_argument('-f', '--from-dir')
         self.parser.add_argument('-t', '--to-dir')
         self.args = self.parser.parse_args()
         os.makedirs(self.args.out_dir, exist_ok=True)
 
 
-def funktion(dump_files, locale):
+def create_rows(dump_files, locale):
     rows = []
     for file in dump_files:
         for element in (file.rulegroups + file.rules):
@@ -46,27 +46,27 @@ def __main__():
     logger = logger_wrapper(cli.parser.prog, cli.args.verbosity)
     logger.debug(f"Starting script...\nInvoked with options: {cli.args}")
     all_path = path.join(cli.args.out_dir, 'all_comments.csv')
-    diff_path = path.join(cli.args.out_dir, 'diff_comments.csv')
+    # diff_path = path.join(cli.args.out_dir, 'diff_comments.csv')
     headers = ['locale', 'file', 'rule_id', 'sub_id', 'tone_tags', 'tag', 'content']
     to_rows, from_rows = [], []
     for locale in LOCALES:
         for repo_name, repo_dir in REPOS.items():
-            from_repo_path = path.join(cli.args.from_dir, repo_dir)
+            # from_repo_path = path.join(cli.args.from_dir, repo_dir)
             to_repo_path = path.join(cli.args.to_dir, repo_dir)
-            from_dump = RuleDump(from_repo_path, locale)
+            # from_dump = RuleDump(from_repo_path, locale)
             to_dump = RuleDump(to_repo_path, locale)
-            to_rows += funktion(to_dump.files, locale)
-            from_rows += funktion(from_dump.files, locale)
+            to_rows += create_rows(to_dump.files, locale)
+            # from_rows += create_rows(from_dump.files, locale)
     df = DataFrame(to_rows, columns=headers).sort_values(['locale', 'rule_id', 'sub_id'])
     logger.info(df.describe())
     all_comments = open(all_path, 'w', encoding='utf-8')
     all_comments.write(df.to_csv(index=False, line_terminator="\n"))
 
-    diff_rows = [tr for tr in to_rows if tr not in from_rows]
-    df2 = DataFrame(diff_rows, columns=headers).sort_values(['locale', 'rule_id', 'sub_id'])
-    logger.info(df2.describe())
-    diff_comments = open(diff_path, 'w', encoding='utf-8')
-    diff_comments.write(df2.to_csv(index=False, line_terminator="\n"))
+    # diff_rows = [tr for tr in to_rows if tr not in from_rows]
+    # df2 = DataFrame(diff_rows, columns=headers).sort_values(['locale', 'rule_id', 'sub_id'])
+    # logger.info(df2.describe())
+    # diff_comments = open(diff_path, 'w', encoding='utf-8')
+    # diff_comments.write(df2.to_csv(index=False, line_terminator="\n"))
 
 
 __main__()
